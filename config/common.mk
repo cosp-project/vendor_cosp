9be36ba6ed2c3d38f0966defb2c12bb20d78b390
edi194 <misiakadrian@gmail.com>
2019-01-21 09:06:42 +0100

    vendor: restoring old packages

--- config/common.mk
+++ config/common.mk
@@ -1,4 +1,4 @@
-PRODUCT_BRAND ?= PixelExperience
+PRODUCT_BRAND ?= COSP
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
@@ -124,10 +124,7 @@ PRODUCT_PACKAGES += \
 	
 # COSP packages
 PRODUCT_PACKAGES += \
-	messaging \
 	Calendar \
-	Contacts \
-	Dialer \
 	Gallery2 \
 	LatinIME \
 	Snap \
