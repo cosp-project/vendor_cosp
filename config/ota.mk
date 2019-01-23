eaac6bf2366a9b1b81e1f8a0ce0c0a19ab52dffe
edi194 <misiakadrian@gmail.com>
2019-01-23 10:56:58 +0100

    vendor: fix ota config

--- config/ota.mk
+++ config/ota.mk
@@ -14,15 +14,15 @@ endif
 #Device name
 DEVICE := $(subst cosp_,,$(TARGET_PRODUCT))
 
-ifneq ($(OTA_TYPE),Unofficial)
+ifneq ($(OTA_TYPE),UNOFFICIAL)
 # OTA app
 PRODUCT_PACKAGES += \
     Updates
 
 # OTA Configuration
 $(shell echo -e "OTA_Configuration\n \
-ota_beta=https://raw.githubusercontent.com/despOSitoROM/OTAconfig/pie/ota_$(DEVICE)_beta.xml\n \
-ota_official=https://raw.githubusercontent.com/despOSitoROM/OTAconfig/pie/ota_$(DEVICE)_official.xml\n \
+ota_beta=https://raw.githubusercontent.com/cosp-project/OTAconfig/pie/ota_$(DEVICE)_beta.xml\n \
+ota_official=https://raw.githubusercontent.com/cosp-project/OTAconfig/pie/ota_$(DEVICE)_official.xml\n \
 device_name=org.cosp.device\n \
 release_type=Pie\n \
 version_source=org.cosp.version\n \
