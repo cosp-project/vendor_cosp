7f752adef4d920b8f54909aad5910d744246284b
Adrian <misiakadrian@gmail.com>
2019-05-11 11:19:46 +0200

    vendor: ignore by default neverallows again

--- build/core/config.mk
+++ build/core/config.mk
@@ -3,5 +3,10 @@ BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/cosp/build/core/system_rro.mk
 # Rules for MTK targets
 include $(TOPDIR)vendor/cosp/build/core/mtk_target.mk
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Rules for QCOM targets
 include $(TOPDIR)vendor/cosp/build/core/qcom_target.mk
